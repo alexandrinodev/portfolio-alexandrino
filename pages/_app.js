b6cf0887847601224e9f614949ddd63a5c0cb3c7
Alex <alexbrunoalexandrino@gmail.com>
2023-08-30 23:57:31 -0400

    Feature: created struct

---
+++ pages/_app.js
@@ -0,0 +1,28 @@
+import "../styles/globals.css";
+
+//components
+import Layout from "../components/Layout";
+
+
+// router
+import { useRouter } from "next/router";
+
+//framer motion
+import { AnimatePresence, motion } from "framer-motion";
+import Transition from "../components/Transition";
+
+function MyApp({ Component, pageProps }) {
+    const router = useRouter();
+    return (
+        <Layout>
+            <AnimatePresence mode="wait">
+              <motion.div key={router.route} className="h-full ">
+                <Transition />
+                <Component {...pageProps} />
+              </motion.div>
+            </AnimatePresence>
+        </Layout>
+    );
+}
+
+export default MyApp;
