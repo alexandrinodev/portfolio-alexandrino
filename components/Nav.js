b30abef03c97f306f28f75930e541f4be255660c
Alex <alexbrunoalexandrino@gmail.com>
2023-09-03 17:56:50 -0400

    Feature: Particles and Avatar

--- components/Nav.js
+++ components/Nav.js
@@ -38,7 +38,7 @@ const Nav = () => {
     return (
         <nav
             className=" flex flex-col items-center xl:justify-center  gap-y-4 fixed h-max
-      bottom-0 mt-auto xl:right-[2%] top-0 w-full xl:w-16 xl:max-w-md xl:h-screen"
+            bottom-0 mt-auto xl:right-[2%] top-0 w-full xl:w-16 xl:max-w-md xl:h-screen z-50"
         >
             {/* inner */}
             <div
@@ -52,23 +52,24 @@ const Nav = () => {
                             className={`${
                                 link.path === pathname && `text-accent`
                             } relative flex items-center group hover:text-accent transition-all
-                          duration-300`}
+                            duration-300`}
                             href={link.path}
                             key={index}
                         >
                             {/* tooltip */}
                             <div className=" absolute pr-14 right-0 hidden xl:group-hover:flex">
-                                <div 
-                                  className="bg-white relative flex text-primary items-center
-                                  p-[6px] rounded-[3px]">
+                                <div
+                                    className="bg-white relative flex text-primary items-center
+                                    p-[6px] rounded-[3px]"
+                                >
                                     <div className="text-[12px] leading-none font-semibold capitalize">
                                         {link.name}
                                     </div>
                                     {/* triangle */}
-                                    <div className=" border-solid border-l-white border-l-8
-                                    border-y-transparent border-y-[6px] border-r-0 absolute right-2">
-                                        
-                                    </div>
+                                    <div
+                                        className=" border-solid border-l-white border-l-8
+                                    border-y-transparent border-y-[6px] border-r-0 absolute right-2"
+                                    ></div>
                                 </div>
                             </div>
                             {/* tooltip */}
